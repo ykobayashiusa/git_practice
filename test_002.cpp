8e6c98f8a7351ece1b75d2ccba4805686911c2f9
Yutaka Kobayashi <ykobayashiusa@gmail.com>
2021-08-31 09:05:14 +0000

    test

--- test_002.cpp
+++ test_002.cpp
@@ -1,5 +1,9 @@
+// use g++ to compile in iSH
+
 #include <iostream>
+#include<cmath>
+#include<vector>
 
 int main(){
-	std::cout << "cat" << std::endl;
+	std::cout << "cat " << 1+1 <<  std::endl;
 }
